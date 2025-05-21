ac4f883192c02019b7f858dd8efc67f7caf67ab8
Leandro <159966014+Leozz7@users.noreply.github.com>
2025-05-21 20:13:06 -0300

    Parametrizando Comportamentos pt.02.2

--- src/main/java/com/comportamento/teste/teste03.java
+++ src/main/java/com/comportamento/teste/teste03.java
@@ -6,10 +6,9 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 import java.util.function.Predicate;
 
-public class teste02 {
+public class teste03 {
     private static List<Carro> c = List.of(new Carro("Audi", "Preto", 2012), new Carro("Meca", "Azul", 2018), new Carro("Nivus", "Cinza", 2024));
 
     public static void main(String[] args) {
@@ -19,14 +18,14 @@ public class teste02 {
         System.out.println(filtrarCarro(c, car -> car.getNome().equalsIgnoreCase("Meca")));
     }
 
-    private static @NotNull List<Carro> filtrarCarro(@NotNull List<Carro> c, CarPredicate carPredicate) {
-        List<Carro> cFiltrados = new ArrayList<>();
-
-        for (Carro car : c) {
-            if (carPredicate.test((car))) {
-                cFiltrados.add(car);
+    private static <T> List<T> filtrarCarro(List<T> l, Predicate<T> predicate) {
+        List<T> cFiltrados = new ArrayList<>();
+        for (T t : l) {
+            if (predicate.test(t)) {
+                cFiltrados.add(t);
             }
         }
+
         return cFiltrados;
     }
 }
