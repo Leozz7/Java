70ec84f6f8365a352125671f5b1d777cd2a9a78b
Leandro <159966014+Leozz7@users.noreply.github.com>
2025-05-28 21:46:55 -0300

    Streams p.01.

--- src/main/java/com/streams/test/test01.java
+++ src/main/java/com/streams/test/test01.java
@@ -10,7 +10,7 @@ public class test01 {
     public static void main(String[] args) {
         List<LigthNovel> lm = new ArrayList<>(List.of(
                 new LigthNovel("Tensei Shittara", 8.99),
-                new LigthNovel("Overload", 3.99),
+                new LigthNovel("Overload", 2.99),
                 new LigthNovel("Violet Evergarden", 5.99),
                 new LigthNovel("No Game", 2.99),
                 new LigthNovel("Tensei Shittara", 8.99)
