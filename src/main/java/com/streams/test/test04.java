570f16139eb6872fe64ed24181b8373a6fec5f39
Leandro <159966014+Leozz7@users.noreply.github.com>
2025-05-30 11:50:51 -0300

    Streams p.2. | FlatMap

---
+++ src/main/java/com/streams/test/test04.java
@@ -0,0 +1,31 @@
+package com.streams.test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class test04 {
+    public static void main(String[] args) {
+        List<List<String>> dev = new ArrayList<>();
+        List<String> webDesigners = List.of("Juliano Souza", "Mariana Oliveira", "Mauricio Lemos");
+        List<String> desenvolvedores = List.of("Leandro Sena", "Luan Magalhães", "Rodrigo B");
+        List<String> alunos = List.of("Gustavo Lima", "Mauricio Miereles", "Otavio Oliveira");
+        dev.add(webDesigners);
+        dev.add(desenvolvedores);
+        dev.add(alunos);
+
+        dev.stream().
+                flatMap(Collection::stream)
+                .forEach(System.out::println);
+
+        System.out.println("+=======");
+
+
+        for (List<String> list : dev) {
+            for (String pe : list) {
+                System.out.println(pe);
+            }
+        }
+    }
+}
