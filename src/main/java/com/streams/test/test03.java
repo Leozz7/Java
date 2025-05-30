c17f4aea64e1d2ca841995e31814b703589278df
Leandro <159966014+Leozz7@users.noreply.github.com>
2025-05-30 11:40:41 -0300

    Streams p.1.2

--- src/main/java/com/streams/test/test03.java
+++ src/main/java/com/streams/test/test03.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-public class test02 {
+public class test03 {
     private static List<LigthNovel> lm = new ArrayList<>(List.of(
             new LigthNovel("Tensei Shittara", 8.99),
             new LigthNovel("Overload", 2.99),
@@ -16,10 +16,18 @@ public class test02 {
     ));
 
     public static void main(String[] args) {
-        ArrayList<Integer> num = new ArrayList<>();
         lm.stream()
-                .sorted(Comparator.comparing(LigthNovel::getNome))
-                .distinct()
+                .map(LigthNovel::getNome)
                 .forEach(System.out::println);
+
+        long count = lm.stream()
+                .filter(lm -> lm.getPreco() < 5)
+                .count();
+
+        lm.stream().
+                distinct()
+                .filter(lm -> lm.getPreco() < 4);
+
+        System.out.println(count);
     }
 }
