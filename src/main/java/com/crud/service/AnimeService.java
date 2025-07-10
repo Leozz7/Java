723c22a09fcb54cee7d5d0747a7f473102ae320e
Leandro <159966014+Leozz7@users.noreply.github.com>
2025-07-10 10:41:51 -0300

    CRUD | Anime Save Service

--- src/main/java/com/crud/service/AnimeService.java
+++ src/main/java/com/crud/service/AnimeService.java
@@ -1,4 +1,10 @@
 package com.crud.service;
 
+import com.crud.model.Anime;
+import com.crud.repository.AnimeRepository;
+
 public class AnimeService {
+    public static void save(Anime a) {
+        AnimeRepository.save(a);
+    }
 }
